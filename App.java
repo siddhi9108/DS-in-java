2f6ca4d183019e116d808081942f36097c27a0e1
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-10-25 23:31:24 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,19 +1,28 @@
-package AbstractDataTypes;
+package algo.recursion.siddhi;
 
 public class App {
 
 	public static void main(String[] args) {
-		Counter ob = new Counter("myCounter");
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		ob.increment();
-		System.out.println(ob.getCurrentValue());
+		System.out.println(recursiveLinearSearch(new int[] 
+				{ 4, 48, 23, 12, 33, 44, 55, 66 }, 0, 3));
+		// reduceByOne(12);
 	}
 
+	public static void reduceByOne(int x) {
+		if (x >= 0) { // Base Case
+			reduceByOne(x - 1);
+		}
+		System.out.println("Completed: " + x);
+	}
+
+	public static int recursiveLinearSearch(int[] a, int i, int x) {
+		if (i > a.length - 1) {
+			return -1;
+		} else if (a[i] == x) {
+			return i;
+		} else {
+			System.out.println("index at: " + i);
+			return recursiveLinearSearch(a, i + 1, x);
+		}
+	}
 }
