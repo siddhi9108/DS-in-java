e25b82e39b87c33824cfb33d13f8eda5c3030696
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-10-25 23:33:18 -0600

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,13 @@
+package binarySearchTree;
+
+public class Node {
+	int key;
+	String value;
+	Node LChild, RChild;
+
+	public Node(int key, String value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+}
