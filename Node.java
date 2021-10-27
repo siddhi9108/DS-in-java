961a9855cf62e9637cdff3ab162e98e9fc11a692
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-10-27 06:22:44 -0600

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,11 @@
+package doublyLinkedLists;
+
+public class Node {
+	public int data;
+	public Node next;
+	public Node previous;
+
+	public void displayNode() {
+		System.out.print("{" + data + "}");
+	}
+}
