cc24cfe2703f8c2729eeb2341be32ab0bfc68112
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-11-02 00:02:01 -0600

    Add files via upload

---
+++ Zoo.java
@@ -0,0 +1,12 @@
+package siddhi.course2.ds.algo;
+
+public class Zoo {
+
+	public static void main(String[] args) {
+		Animal ob = new Animal(12, 33, 'F');
+		ob.speak();
+		Bird ob1 = new Bird();
+		ob1.fly();
+	}
+
+}
