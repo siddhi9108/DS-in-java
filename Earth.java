722290324748b5819de29eb1a640eedc1a6f698a
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-11-04 12:40:37 -0600

    Add files via upload

---
+++ Earth.java
@@ -0,0 +1,27 @@
+
+public class Earth {
+	public static void main(String args[]) {
+		Human h1 = new Human("Siddhi", 21, 75, "Blue");
+		Human h2 = new Human("Ruturaj", 20, 80, "Black");
+		Human h3 = new Human("M", 43, 45, "Brown");
+
+		h1.speak();
+		h2.speak();
+		h3.speak();
+		// Human siddhi;
+		// Human siddhi = new Human();
+		// siddhi.age = 21;
+		// siddhi.eyeclr = "black";
+		// siddhi.height = 51;
+		// siddhi.name = "Siddhi";
+		// siddhi.speak();
+
+		// Human ob = new Human();
+		// ob.age = 20;
+		// ob.eyeclr = "brown";
+		// ob.height = 55;
+		// ob.name = "Ruturaj";
+		// ob.speak();
+	}
+
+}
